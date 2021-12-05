1cc466eab337c0b82376b4fbe019ae8a021cc1b9
pedroandradee <pedro.h8237@gmail.com>
2021-12-05 11:22:32 -0300

    commit

--- index.js
+++ index.js
@@ -32,8 +32,6 @@ io.on("connection", (socket) => {
 
     //send and get messages
     socket.on("sendNotification", ({senderId, receiverId}) => {
-        const user = getUser(receiverId);
-        console.log(receiverId);
         if(user){
             io.to(user.socketId).emit("getNotification", {
                 senderId
